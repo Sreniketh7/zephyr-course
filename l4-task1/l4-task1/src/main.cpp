d1325746db82a74d4284e6146798e5931113242c
Sreniketh7 <sreniketh13@gmail.com>
2026-05-14 15:15:11 +0200

    l4-task1
    l4-task1

---
+++ l4-task1/l4-task1/src/main.cpp
@@ -0,0 +1,33 @@
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+#define SLEEP_TIME_MS 1000
+
+/* The devicetree node identifier for the "led0" alias. */
+#define LED_NODE DT_ALIAS(app_led)
+
+//#define LED_NODE DT_NODELABEL(green_led_2) //Another method to get the node identifier for the led_2 node
+
+//#define LED_NODE DT_PATH(leds,green_led_2) //Another method to get the node identifier for the led_2 node using the path defined in the devicetree
+
+//#define LED_NODE DT_ALIAS(user_button) //Another method to get the node identifier for the user_button node using the alias defined in the devicetree
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
+
+int main(void)
+{
+    bool led_state = true;
+
+    if (!gpio_is_ready_dt(&led)) return 0;
+
+    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
+
+    while (1) {
+        if (gpio_pin_toggle_dt(&led) < 0) return 0;
+        
+        k_msleep(CONFIG_APP_HEARTBEAT_PERIOD_MS);    }
+    return 0;
+}
